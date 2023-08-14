b77ac306dda86310c35755598edd7c3c894dd1b6
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2023-08-14 13:04:23 +0200

    reworked permission checking implementation

--- app/src/main/java/com/dimagi/biometric/fragments/BaseMatchFragment.java
+++ app/src/main/java/com/dimagi/biometric/fragments/BaseMatchFragment.java
@@ -1,7 +1,10 @@
 package com.dimagi.biometric.fragments;
 
-import android.content.DialogInterface;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.Manifest;
 
@@ -11,13 +14,13 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.dimagi.biometric.R;
 
@@ -27,7 +30,7 @@ import com.dimagi.biometric.R;
 public abstract class BaseMatchFragment extends Fragment {
     private static final String TAG = "BIOMETRIC";
 
-    private Toast permissionToast;
+    private final String PREV_RATIONALE_KEY = "prev_rationale";
 
     protected abstract void handleStartCapture();
     protected abstract void handleCancelCapture();
@@ -39,13 +42,13 @@ public abstract class BaseMatchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main_menu, container, false);
-
         Button startButton = view.findViewById(R.id.start_capture_button);
         startButton.setOnClickListener(v -> {
             if (ContextCompat.checkSelfPermission(
                     requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
                 handleStartCapture();
             } else if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                savePreviousRationale();
                 showPermissionAlertDialog();
             } else {
                 requestPermissionLauncher.launch(Manifest.permission.CAMERA);
@@ -53,7 +56,6 @@ public abstract class BaseMatchFragment extends Fragment {
         });
         Button cancelButton = view.findViewById(R.id.cancel_capture_button);
         cancelButton.setOnClickListener(v -> handleCancelCapture());
-
         return view;
     }
 
@@ -61,47 +63,61 @@ public abstract class BaseMatchFragment extends Fragment {
             registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
         if (isGranted) {
             handleStartCapture();
-        } else {
-            showPermissionToast();
+        } else if (isPermissionDeniedPermanently()) {
+            requestOpenAppSettings();
         }
     });
 
     private void showPermissionAlertDialog() {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(requireContext());
+        alertDialogBuilder.setTitle(getText(R.string.camera_permission_required));
         alertDialogBuilder.setMessage(getText(R.string.camera_permission_rationale));
-        alertDialogBuilder.setPositiveButton(getText(R.string.confirm), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                requestPermissionLauncher.launch(Manifest.permission.CAMERA);
-            }
-        });
-
-        alertDialogBuilder.setNegativeButton(getText(R.string.cancel), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-            }
-        });
-
-        AlertDialog alertDialog = alertDialogBuilder.create();
-        alertDialog.show();
+        alertDialogBuilder.setPositiveButton(getText(R.string.confirm), (dialog, which) -> requestPermissionLauncher.launch(Manifest.permission.CAMERA));
+        alertDialogBuilder.setNegativeButton(getText(R.string.cancel), null);
+        alertDialogBuilder.create().show();
     }
 
-    private void showPermissionToast() {
-        CharSequence text = getText(R.string.camera_permission_required);
-        if (permissionToast != null) {
-            permissionToast.cancel();
-        }
-        permissionToast = Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT);
-        permissionToast.show();
+    private void requestOpenAppSettings() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(requireContext());
+        alertDialogBuilder.setTitle(getText(R.string.go_to_settings));
+        alertDialogBuilder.setMessage(getText(R.string.camera_permission_settings));
+        alertDialogBuilder.setPositiveButton(getText(R.string.confirm), (dialogInterface, i) -> {
+            Intent intent = new Intent();
+            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            intent.addCategory(Intent.CATEGORY_DEFAULT);
+            intent.setData(Uri.parse("package:" + requireContext().getPackageName()));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            requireContext().startActivity(intent);
+        });
+        alertDialogBuilder.setNegativeButton(getText(R.string.cancel), null);
+        alertDialogBuilder.create().show();
     }
 
     protected void handleErrorMessage(String error) {
         try {
-            TextView errorText = getView().findViewById(R.id.error_text);
+            TextView errorText = requireView().findViewById(R.id.error_text);
             errorText.setText(error);
         } catch (NullPointerException ex) {
             Log.e(TAG, "Null pointer on trying to set error message");
         }
+    }
 
+    /**
+     * After the second denial shouldShowRequestPermissionRationale will be false, so we need to
+     * save that this was true to only show the dialog asking the user to navigate
+     * to the app settings after they have denied the permission twice.
+     */
+    private void savePreviousRationale() {
+        SharedPreferences sharedPref = requireActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putBoolean(PREV_RATIONALE_KEY, true);
+        editor.apply();
+    }
+
+    private boolean isPermissionDeniedPermanently() {
+        SharedPreferences sharedPref = requireActivity().getPreferences(Context.MODE_PRIVATE);
+        boolean prevRationale = sharedPref.getBoolean(PREV_RATIONALE_KEY, false);
+        boolean hasRationale = shouldShowRequestPermissionRationale(Manifest.permission.CAMERA);
+        return !hasRationale && prevRationale;
     }
 }
