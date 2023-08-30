1f823b4fb52db1357a6330daab89fcd2ab6773ff
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2023-08-30 16:14:38 +0200

    implement verification changes

--- app/src/main/java/com/dimagi/biometric/activities/VerifyActivity.java
+++ app/src/main/java/com/dimagi/biometric/activities/VerifyActivity.java
@@ -1,36 +1,39 @@
 package com.dimagi.biometric.activities;
 
-import android.os.Bundle;
+import com.dimagi.biometric.R;
 
 import org.commcare.commcaresupportlibrary.identity.IdentityResponseBuilder;
 import org.commcare.commcaresupportlibrary.identity.model.MatchResult;
 import org.commcare.commcaresupportlibrary.identity.model.MatchStrength;
 
+import java.util.ArrayList;
+
 import Tech5.OmniMatch.MatcherCommon;
 
 public class VerifyActivity extends BaseActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        MatcherCommon.Record caseRecord = getRecordFromIntent();
-        if (caseRecord != null && caseId != null) {
+    protected void onCaptureSuccess(MatcherCommon.Record activeRecord) {
+        if (templateStr == null) {
+            return;
+        }
+        MatcherCommon.Record caseRecord = templateViewModel.getRecordFromTemplateStr(templateStr);
+        if (caseRecord != null) {
             templateViewModel.insertRecord(caseRecord, caseId);
         }
-    }
-
-    @Override
-    protected void onCaptureSuccess(MatcherCommon.Record activeRecord) {
         float score = templateViewModel.verifyRecord(activeRecord, caseId);
-        IdentityResponseBuilder.verificationResponse(
-                caseId, new MatchResult((int) (score * 100), getMatchStrength(score))
-        ).finalizeResponse(this);
+        finishWorkflow(score);
     }
 
     @Override
     protected void onCaptureCancelled() {
+        finishWorkflow(0f);
+    }
+
+    private void finishWorkflow(float score) {
+        int confidencePercentage = (int)(score * 100);
+        MatchStrength matchStrength = getMatchStrength(score);
         IdentityResponseBuilder.verificationResponse(
-                caseId, new MatchResult(0, MatchStrength.ONE_STAR)
+                caseId, new MatchResult(confidencePercentage, matchStrength)
         ).finalizeResponse(this);
     }
 
